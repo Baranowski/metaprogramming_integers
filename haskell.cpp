c9b695dad6d3420b0deb4ad4b5a96102625419fa
Wojciech Baranowski <w.baranowski@gmail.com>
2017-02-18 17:02:40 +0000

    ZipWithDefault

--- haskell.cpp
+++ haskell.cpp
@@ -1,22 +1,23 @@
 #include <type_traits>
 
-typedef unsigned char uint8_t;
 
 // Bit definition
 
 struct Zero {};
 struct One {};
 
+
 // Generic list definition
 
 // The `:` operator known from Haskell
-template <class HEAD, class TAIL>
+template <class Head, class Tail>
 struct Cons {
-    typedef HEAD head;
-    typedef TAIL tail;
+    typedef Head head;
+    typedef Tail tail;
 };
 
 struct Leaf {};
+
 
 // Reversal
 
@@ -51,6 +52,117 @@ namespace {
     static_assert(std::is_same<r011::tail::tail::head, Zero>::value, "a");
     static_assert(std::is_same<r011::tail::tail::tail, Leaf>::value, "a");
 };
+
+
+// Pair - Syntactic sugar for two-element list
+
+template<class Left, class Right>
+struct Pair : public Cons<Left, Cons<Right, Leaf> > {
+    typedef Left left;
+    typedef Right right;
+};
+
+// ZipWithDefault
+
+template<class ListA, class ListB, class Default, class Agg>
+struct ZipWithDefaultHelper{
+    typedef
+        typename ZipWithDefaultHelper<
+            typename ListA::tail,
+            typename ListB::tail,
+            Default,
+            Cons<
+                Pair<typename ListA::head, typename ListB::head>,
+                Agg
+            >
+        >::result
+    result;
+};
+
+template<class Default, class Agg>
+struct ZipWithDefaultHelper<Leaf, Leaf, Default, Agg> {
+    typedef
+        typename Reverse<Agg>::result
+    result;
+};
+
+template<class ListB, class Default, class Agg>
+struct ZipWithDefaultHelper<Leaf, ListB, Default, Agg> {
+    typedef
+        typename ZipWithDefaultHelper<
+            Leaf,
+            typename ListB::tail,
+            Default,
+            Cons<
+                Pair<Default, typename ListB::head>,
+                Agg
+            >
+        >::result
+    result;
+};
+
+template<class ListA, class Default, class Agg>
+struct ZipWithDefaultHelper<ListA, Leaf, Default, Agg> {
+    typedef
+        typename ZipWithDefaultHelper<
+            typename ListA::tail,
+            Leaf,
+            Default,
+            Cons<
+                Pair<typename ListA::head, Default>,
+                Agg
+            >
+        >::result
+    result;
+};
+
+template<class ListA, class ListB, class Default>
+using ZipWithDefault = ZipWithDefaultHelper<ListA, ListB, Default, Leaf>;
+
+namespace {
+    typedef Cons<One, Cons<Zero, Cons<One, Leaf>>> a101;
+    typedef Cons<Zero, Cons<Zero, Cons<One, Cons<One, Leaf>>>> b0011;
+    typedef typename ZipWithDefault<a101, b0011, Zero>::result zipped;
+    static_assert(std::is_same<
+        zipped::head,
+        Pair<One, Zero>
+    >::value, "a");
+    static_assert(std::is_same<
+        zipped::tail::head,
+        Pair<Zero, Zero>
+    >::value, "a");
+    static_assert(std::is_same<
+        zipped::tail::tail::head,
+        Pair<One, One>
+    >::value, "a");
+    static_assert(std::is_same<
+        zipped::tail::tail::tail::head,
+        Pair<Zero, One>
+    >::value, "a");
+    static_assert(std::is_same<
+        zipped::tail::tail::tail::tail,
+        Leaf
+    >::value, "a");
+        
+};
+
+// Addition
+
+/*
+template<class A, class B>
+struct Add {
+    typedef
+        typename FoldL<
+            AddH,
+            typename ZipWithDefault<
+                typename Reverse<A>::result,
+                typename Reverse<B>::result,
+                Zero
+            >::result,
+            Leaf
+    result;
+};
+*/
 
 int main(int argc, char *argv[]) {
     return 0;
